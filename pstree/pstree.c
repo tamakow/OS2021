dc281887d03fb748f236c02f01a31606100626a4
tracer-nju <tracer@nju.edu.cn>
2021-03-10 09:53:44 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: efc63dbfda8841a79a836d9d9cb46b48   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-43-generic       Architecture: x86-64  09:53:44 up 35 min,  1 user,  load average: 0.66, 0.81, 0.90

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,12 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <getopt.h>
 
+struct option options[] = {
+  {"show-pids", 0, NULL, 'p'},
+  {"numeric-sort", 0, NULL, 'n'},
+  {"version", 0, NULL, 'V'},
+  {0, 0, 0, 0}
+};
+
+void usage(){
+  printf("\033[31mPlease input valid arguments\033[0m \n"
+  " Usage: pstree [-p, --show-pids] [-n, --numeric-sort] [-V, --version]\n");
+  exit(1);
+}
+
+void print_version(){
+  printf("\033[32m successfully identify -V --version\033[01m ");
+}
 
 int main(int argc, char *argv[]) {
+  int c;
+
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
+    while((c = getopt_long(argc,argv,"pnV", options, NULL)) != -1) {
+      switch (c) {
+        case 'V':
+          print_version();
+          break;
+        case 'p':
+          printf("\033[31m successfully identify -p --show-pids\033[01m \n");
+          break;
+        case 'n':
+          printf("\033[31m successfully identify -n --numeric-sort\033[01m \n");
+          break;
+        default: usage();
+      }
+    }
   }
   assert(!argv[argc]);
   return 0;
