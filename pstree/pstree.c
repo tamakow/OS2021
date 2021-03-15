fd4cdc919b33faf427c16bd5567f203f4925a83d
tracer-nju <tracer@nju.edu.cn>
2021-03-15 22:54:17 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 2486e47c75354860b9ea3f148b66495d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-43-generic       Architecture: x86-64  22:54:17 up  9:01,  1 user,  load average: 1.03, 0.84, 0.68

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -145,10 +145,11 @@ static void add_process (pid_t pid, char* comm, char state, pid_t ppid) {
   
   new_proc->parent = parent;
   new_proc->next = parent->child;
-  if(new_proc->next) printf("%d\n",new_proc->next->pid);
   parent->child = new_proc;
   printf("%d %d\n",ppid, pid);
-  if(parent->child) printf("Child exists\n");
+  if(parent->child) {
+    printf("Child exists\n %d",parent->child->pid);
+  }
   // parent->child = new_proc;//bugs here
   
 }
