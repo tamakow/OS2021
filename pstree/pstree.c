940977072fbb31da4c986abb53df4620410f0622
tracer-nju <tracer@nju.edu.cn>
2021-03-15 19:51:53 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 2486e47c75354860b9ea3f148b66495d   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-43-generic       Architecture: x86-64  19:51:53 up  5:59,  1 user,  load average: 0.19, 0.40, 0.43

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -121,7 +121,7 @@ static void read_stat (int pid) {
    sprintf(path, "%s/%d/stat", PROC_BASE, pid);
    if((fp = fopen(path, "r")) != NULL) {
      fscanf(fp, "%d (%[^)]) %c %d",&pid,comm,&state,&ppid);
-     printf("%c\n", state);
+     printf("%s %c\n",comm, state);
      fclose(fp);
    }
 }
