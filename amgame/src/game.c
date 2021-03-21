0e3d84872380843bb3be69e4a29efc56ddb61137
tracer-nju <tracer@nju.edu.cn>
2021-03-21 14:52:28 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 4f9ba8535b874c42b2e65de3aecd34c8   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  14:52:28 up  4:03,  1 user,  load average: 1.08, 0.73, 0.85

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -22,10 +22,14 @@
 #define green  "\033[1;32m"
 #define close     "\033[0m"
 
-struct character {
+struct BALL {
   int x, y, v, t;
 } ball;
 
+struct BOARD {
+  int x, y;
+  int len;
+} board;
 
 static void video_init();
 // static void new_ball();
@@ -41,17 +45,10 @@ int main(const char *args) {
   ioe_init();
   video_init();
 
-  // puts("mainargs = \"");
-  // puts(args); // make run mainargs=xxx
-  // puts("\"\n");
-
-  // splash();
-
   puts(red"'ESC' to exit this game\n"close);
   puts(green"Please press 'A' or 'D' to move the board\n"close);
 
-
-  puts("Press any key to see its key code...\n");
+  
   while (1) {
     print_key();
   }
@@ -70,6 +67,28 @@ static void video_init() {
   screen_h = io_read(AM_GPU_CONFIG).height;
   screen_w = io_read(AM_GPU_CONFIG).width;
 
+  uint32_t pixels[SIDE * SIDE];
+  for (int i = 0; i < SIDE * SIDE; ++ i) {
+    pixels[i] = COL_BLUE;
+    blank[i] = COL_Cyan;
+  }
+  //draw the backgroud
+  for (int x = 0; x * SIDE <= screen_w; ++ x) {
+    for (int y = 0; y * SIDE <= screen_h; ++ y) {
+      io_write(AM_GPU_FBDRAW, x*SIDE, y*SIDE, blank, SIDE, SIDE, false);
+    }
+  }
+
+  //draw the board
+  board.x = 3 * (screen_w / 8) / SIDE;
+  board.y = screen_h - SIDE;
+  board.len = 2 * (screen_w / 8) / SIDE;
+  for (int x = board.x; x <= board.x + board.len; ++ x) {
+    io_write(AM_GPU_FBDRAW, x * SIDE, board.y, pixels, SIDE, SIDE, false);
+  }  
+}
+
+
   //draw a screen of balls 
   // uint32_t pixels[SIDE * SIDE];
   // for (int x = 0; x < SIDE; ++ x) {
@@ -83,20 +102,3 @@ static void video_init() {
   //     io_write(AM_GPU_FBDRAW, x*SIDE, y*SIDE, pixels, SIDE, SIDE, false);
   //   }
   // }
-
-
-  uint32_t pixels[SIDE * SIDE];
-  for (int i = 0; i < SIDE * SIDE; ++ i) {
-    pixels[i] = COL_BLUE;
-    blank[i] = COL_Cyan;
-  }
-  for (int x = 0; x * SIDE <= screen_w; ++ x) {
-    for (int y = 0; y * SIDE <= screen_h; ++ y) {
-      io_write(AM_GPU_FBDRAW, x*SIDE, y*SIDE, blank, SIDE, SIDE, false);
-    }
-  }
-
-  for (int x = 3* (screen_w / 8) / SIDE; x * SIDE <= 5 * (screen_w / 8); ++ x) {
-    io_write(AM_GPU_FBDRAW, x * SIDE, screen_h - SIDE, pixels, SIDE, SIDE, false);
-  }  
-}
