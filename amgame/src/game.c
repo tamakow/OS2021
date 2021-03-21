6106feaa4fee68d668ab477fd9190dc87a66e03f
tracer-nju <tracer@nju.edu.cn>
2021-03-21 16:42:34 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 4f9ba8535b874c42b2e65de3aecd34c8   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  16:42:34 up  5:53,  1 user,  load average: 0.64, 0.71, 0.79

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -28,35 +28,44 @@
 
 struct BALL {
   int x, y, v, t;
+  bool exist;
 } ball;
 
 struct BOARD {
-  int y;
+  int height;
   int head,tail;
 } board;
 
 static void video_init();
-static void update_board();
-// static void new_ball();
+static void update_screen();
+static void update_state();
+static void new_ball();
+static void ball_init();
 static int min(int a,int b);
-// static int randint(int l,int r);
+static int randint(int l,int r);
 
 
 static int screen_w, screen_h;
 static uint32_t blank[SIDE * SIDE];
 static uint32_t Board[SIDE * SIDE];
+static uint32_t Ball[SIDE * SIDE];
 
 // Operating system is a C program!
 int main(const char *args) {
   ioe_init();
   video_init();
+  ball_init();
 
   puts(red"'ESC' to exit this game\n"close);
   puts(green"Please press 'A' or 'D' to move the board\n"close);
 
   int frame1 = 0, frame2 = 0;
   while (1) {
-    frame1 = io_read(AM_TIMER_UPTIME).us/ (1000000 / FPS);
+    int frame = io_read(AM_TIMER_UPTIME).us/ (1000000 / FPS);
+
+    for (; frame1 < frame; ++frame1) {
+      update_state();
+    }
     while(1){
       AM_INPUT_KEYBRD_T event = io_read(AM_INPUT_KEYBRD);
       if (event.keycode == AM_KEY_NONE) break;
@@ -75,7 +84,7 @@ int main(const char *args) {
       }
     }
     if(frame1 > frame2) {
-      update_board();
+      update_screen();
       frame2 = frame1;
     }
   }
@@ -86,9 +95,19 @@ static int min(int a, int b) {
   return (a > b)? b : a; 
 }
 
-// static randint(int l, int r) {
-//   return l + (rand() & 0x7fffffff) % (r - l + 1);
-// }
+static int randint(int l, int r) {
+  return l + (rand() & 0x7fffffff) % (r - l + 1);
+}
+
+static void ball_init(){
+  ball.exist = false;
+  for (int x = 0; x < SIDE; ++ x) {
+    for (int y = 0; y < SIDE; ++ y) {
+      if (((x - SIDE/2)*(x - SIDE/2) + (y - SIDE/2)*(y - SIDE/2)) <= SIDE*SIDE/4)
+        Ball[x * SIDE + y] = COL_RED;
+    }
+  }
+}
 
 static void video_init() {
   screen_h = io_read(AM_GPU_CONFIG).height;
@@ -107,31 +126,37 @@ static void video_init() {
 
   //init the board
   board.head = (LEN / 2 - 2) > 0 ? (LEN / 2 - 2) : 0; 
-  board.y = screen_h - SIDE;
+  board.height = screen_h - SIDE;
   board.tail = (board.head + 4) < LEN ? board.head + 4 : 1;
-  update_board(); 
-}
-
-static void update_board() {
-  for (int x = 0; x * SIDE <= screen_w; ++ x) {
-    io_write(AM_GPU_FBDRAW, x * SIDE, board.y, blank, min(SIDE, screen_w - x * SIDE), min(SIDE, screen_h - board.y), false);
-  } 
   for (int x = board.head; x < board.tail; ++ x) {
-    io_write(AM_GPU_FBDRAW, x * SIDE, board.y, Board, min(SIDE, screen_w - x * SIDE), min(SIDE, screen_h - board.y), false);
+    io_write(AM_GPU_FBDRAW, x * SIDE, board.height, Board, min(SIDE, screen_w - x * SIDE), min(SIDE, screen_h - board.height), false);
   } 
 }
 
+static void update_screen() {
+  //init the screen
+  for (int x = 0; x * SIDE <= screen_w; ++ x) {
+    for (int y = 0; y * SIDE <= screen_h; ++ y) {
+      io_write(AM_GPU_FBDRAW, x * SIDE, y * SIDE, blank, min(SIDE, screen_w - x * SIDE), min(SIDE, screen_h - y * SIDE), false);
+    }
+  } 
+  //update board
+  for (int x = board.head; x < board.tail; ++ x) {
+    io_write(AM_GPU_FBDRAW, x * SIDE, board.height, Board, min(SIDE, screen_w - x * SIDE), min(SIDE, screen_h - board.height), false);
+  } 
+  //update ball
+  io_write(AM_GPU_FBDRAW, ball.x * SIDE, ball.y * SIDE, Ball, min(SIDE, screen_w - ball.x * SIDE), min(SIDE, screen_h - ball.y * SIDE), false);
+}
 
-  //draw a screen of balls 
-  // uint32_t pixels[SIDE * SIDE];
-  // for (int x = 0; x < SIDE; ++ x) {
-  //   for (int y = 0; y < SIDE; ++ y) {
-  //     if (((x - SIDE/2)*(x - SIDE/2) + (y - SIDE/2)*(y - SIDE/2)) <= SIDE*SIDE/4)
-  //       pixels[x*SIDE + y] = COL_WHITE;
-  //   }
-  // }
-  // for (int x = 0; x * SIDE <= screen_h; ++ x) {
-  //   for (int y = 0; y * SIDE <= screen_w; ++ y) {
-  //     io_write(AM_GPU_FBDRAW, x*SIDE, y*SIDE, pixels, SIDE, SIDE, false);
-  //   }
-  // }
+static void new_ball() {
+  ball.t = 0;
+  ball.v = (screen_h - SIDE + 1) / randint(FPS, FPS * 2);
+  ball.x = randint (0, screen_w - SIDE);
+  ball.y = 0;
+  ball.exist = true;
+}
+
+static void update_state() {
+  if(!ball.exist) new_ball();
+}
+
