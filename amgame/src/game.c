6fc7c15143e2a8c8a50e6495632d2f610b1f0e89
tracer-nju <tracer@nju.edu.cn>
2021-03-21 15:44:35 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 4f9ba8535b874c42b2e65de3aecd34c8   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-44-generic       Architecture: x86-64  15:44:35 up  4:55,  1 user,  load average: 1.14, 0.70, 0.68

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -66,7 +66,7 @@ int main(const char *args) {
           board.x -= LEN;
       }
       if (event.keydown && event.keycode == AM_KEY_D) {
-        if(board.x + board.len < 32 * LEN) 
+        if(board.x + board.len < LEN * 32) 
           board.x += LEN;
       }
     }
