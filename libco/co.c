733df13da2589ea936167864d268655acce1708f
tracer-nju <tracer@nju.edu.cn>
2021-03-24 21:02:04 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: d1c486fca99141ff90bff248fb4525de   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  21:02:04 up 36 min,  1 user,  load average: 0.18, 0.19, 0.20

--- libco/co.c
+++ libco/co.c
@@ -1,7 +1,22 @@
 #include "co.h"
 #include <stdlib.h>
 
+enum co_status {
+  CO_NEW = 1, 
+  CO_RUNNING,
+  CO_WAITING,
+  CO_DEAD,
+};
+
 struct co {
+  char *name;
+  void (*func)(void *);
+  void *arg;
+
+  enum co_status status;
+  struct co * waiter;
+  jmp_buf context;
+  uint8_t  stack[STACK_SIZE];
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
