a56567b9782d1fa7719f92c7cdbd4d1e4c8075fe
tracer-nju <tracer@nju.edu.cn>
2021-03-27 19:43:11 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b7153b05d88947e38ba636cd74a3e24b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  19:43:11 up  9:30,  1 user,  load average: 1.69, 1.49, 1.38

--- libco/co.c
+++ libco/co.c
@@ -65,6 +65,7 @@ inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
 }
 
 void free_co(struct co* co) {
+  Log("S");
   if(list == NULL) return;
   struct co *walk = list;
   if(list == co) {
