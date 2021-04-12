d3166a312eefff2196ad0a951b2133d4187433ca
tracer-nju <tracer@nju.edu.cn>
2021-04-12 22:51:10 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 875f790f7340477683d9ea9450024031   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  22:51:10 up  5:51,  1 user,  load average: 0.67, 0.85, 0.81

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -13,12 +13,9 @@ static void *kalloc(size_t size) {
   tmp = 0;
   size_t i = 1;
   while(i < size) i<<=1;
-  printf("page is %d\n head is %d\n",i,(size_t)head);
-  size_t j = ((size_t)head / i + 1)* i;
-  printf("j = %d\n",j);
-  while((uintptr_t)head < j) head++;
+  while((size_t)head % i != 0) head++;
   tmp = size;
-  printf("head is %d\n",(size_t)head);
+  printf("head is %x\n",head);
   release(&lk);
   return head;
 }
