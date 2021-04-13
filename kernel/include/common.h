807a4a5b4a670d070d054b3c46f699f6dd01feea
tracer-nju <tracer@nju.edu.cn>
2021-04-13 23:23:34 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: d5c7639ebf504f239ff3632e9fdc862b   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  23:23:34 up  6:56,  1 user,  load average: 0.54, 0.78, 0.84

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -2,3 +2,9 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <../../abstract-machine/am/src/x86/x86.h>
+
+
+#define DEBUG
+
+
+#include <debug.h>
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -8,7 +8,8 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  pmm->alloc(100);
+  print(FONT_CYAN,"HELLO");
+  Log("HELLO");
   while (1) ;
 }
 
