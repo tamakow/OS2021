cf6839ee490f4eaf780418107dea7f999a1f6c43
tracer-nju <tracer@nju.edu.cn>
2021-04-14 14:13:35 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 18d78fb3e7384da3b30199fc83ee7abf   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-45-generic       Architecture: x86-64  14:13:35 up 7 min,  1 user,  load average: 0.27, 0.45, 0.27

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -3,32 +3,28 @@
 
 //first just try one lock and kalloc
 static void *head;
-static size_t tmp = 0;
 struct spinlock lk;
 static void *kalloc(size_t size) {
   acquire(&lk);
-  if(tmp > 0)
-    head = head + tmp;
   if((uintptr_t)head > (uintptr_t)heap.end){ 
     release(&lk);
     return NULL;
   }
-  tmp = 0;
   size_t i = 1;
   while(i < size) i<<=1;
   // while((size_t)head % i != 0) head++;
-  head = (void *)(((size_t)head / i + 1) * i); 
-  tmp = size;
+  head = (void *)(((size_t)head / i + 1) * i) + size; 
   release(&lk);
-  return head;
+  return head - size;
 }
 
 static void kfree(void *ptr) {
+
 }
 
 static void pmm_init() {
   head = heap.start;
-  initlock(&lk,"lalala");
+  initlock(&lk,"GlobalLock");
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
