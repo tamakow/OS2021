a6d977dcbeae393ec3cdd7672f6fb50c5105ad93
tracer-nju <tracer@nju.edu.cn>
2021-04-18 12:02:01 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: c6a9ce76c60147a6959d10da68980a12   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-49-generic       Architecture: x86-64  12:02:01 up  3:06,  1 user,  load average: 1.40, 0.87, 0.79

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -19,41 +19,5 @@ struct slab {
     struct slab *next;
 };
 
-struct slab* cache_chain[MAX_CPU + 1][NR_ITEM_SIZE + 1];
-
-void slab_init() {
-    for(int i = 0; i < MAX_CPU + 1; ++ i) {
-        for(int j = 0; j < NR_ITEM_SIZE + 1; ++j) {
-            cache_chain[i][j] = NULL;
-        }
-    }
-}
-
-void new_slab(struct slab * sb, int cpu, int item_size) {
-    assert(sb != NULL);
-    sb->cpu           = cpu;
-    sb->item_size     = item_size;
-    sb->max_item_nr   = SLAB_SIZE / sb->item_size;
-    memset(sb->bitmap, 0, sizeof(sb->bitmap));
-    sb->next          = NULL;
-}
-
-//判断该slab是否已满，满了返回true，否则返回false
-bool full_slab(struct slab* sb) {
-    assert(sb != NULL);
-    int block = -1;
-    for(int i = 0; i < 64; ++i) {
-        if(sb->bitmap[i] != UINT64_MAX) {
-            block = i * 64;
-            int cnt = 0;
-            while (cnt < 64 && (sb->bitmap[i] & (1ULL << cnt))) cnt++;
-            block += cnt;
-            break;
-        }
-    }
-    if(block == -1) return true; // 没有必要，因为一定会有不合理的位置空出
-    if(block < sb->max_item_nr) return false;
-    return true;
-}
 
 #endif
