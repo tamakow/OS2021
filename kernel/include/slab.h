535bececa0e2e4eaa2bed88d54cdefa1183089b6
tracer-nju <tracer@nju.edu.cn>
2021-04-18 11:56:22 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: c6a9ce76c60147a6959d10da68980a12   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-49-generic       Architecture: x86-64  11:56:22 up  3:00,  1 user,  load average: 0.58, 0.73, 0.76

--- kernel/include/slab.h
+++ kernel/include/slab.h
@@ -2,17 +2,62 @@
 #define __SLAB_H__
 
 #define  KiB            *(1 << 10)
-#define  PAGE_SIZE      4 KiB
+#define  PAGE_SIZE      (4 KiB)
+#define  NR_SLAB_PAGE   2
+#define  SLAB_SIZE      (NR_SLAB_PAGE * PAGE_SIZE)
+#define  BITMAP_SIZE    64
+#define  NR_ITEM_SIZE   12
+// #define  UINT64_MAX     ((1<<64) - 1) 
+//item size 设置为{2, 4, 8, 16,... ,2^12} 共十二项，故bitmap的最大size应该设置为不小于 SLAB_SIZE / 2 = 3.5 KiB ，这里为了方便，设置为 4 KiB = 64 * BITMAP_SIZE
 
-struct list_head {
-    struct list_head *prev;
-    struct list_head *next;
+struct slab {
+    uint8_t data[SLAB_SIZE - 1 KiB]; // 分配空间，这里给了7kiB
+    int cpu;                 // 所属的cpu
+    int item_size;           // slab 的每个item的大小
+    int max_item_nr;         // 最多可以有的 item数量
+    uint64_t bitmap[BITMAP_SIZE];
+    struct slab *next;
 };
 
-struct kmem_cache {
-    struct list_head slabs_partial;
-    struct list_head slabs_full;
-    struct list_head slabs_free;
-};
+struct slab* cache_chain[MAX_CPU + 1][NR_ITEM_SIZE + 1];
+
+int get_slab_size(int slab_id) {
+    return (1<<slab_id);
+}
+
+void slab_init() {
+    for(int i = 0; i < MAX_CPU + 1; ++ i) {
+        for(int j = 0; j < NR_ITEM_SIZE + 1; ++j) {
+            cache_chain[i][j] = NULL;
+        }
+    }
+}
+
+void new_slab(struct slab * sb, int cpu, int item_size) {
+    assert(sb != NULL);
+    sb->cpu           = cpu;
+    sb->item_size     = item_size;
+    sb->max_item_nr   = SLAB_SIZE / sb->item_size;
+    memset(sb->bitmap, 0, sizeof(sb->bitmap));
+    sb->next          = NULL;
+}
+
+//判断该slab是否已满，满了返回true，否则返回false
+bool full_slab(struct slab* sb) {
+    assert(sb != NULL);
+    int block = -1;
+    for(int i = 0; i < 64; ++i) {
+        if(sb->bitmap[i] != UINT64_MAX) {
+            block = i * 64;
+            int cnt = 0;
+            while (cnt < 64 && (sb->bitmap[i] & (1 << cnt))) cnt++;
+            block += cnt;
+            break;
+        }
+    }
+    if(block == -1) return true; // 没有必要，因为一定会有不合理的位置空出
+    if(block < sb->max_item_nr) return false;
+    return true;
+}
 
 #endif
--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -3,33 +3,85 @@
 #include <slab.h>
 
 
-// just try one lock 
-static void *head;
-static struct spinlock lk;
+//第二次尝试，先只用slab试试，大内存分配先不管（）
+static struct spinlock global_lock;
+static struct spinlock lk; 
+void *head;
+
 
 
 static void *kalloc(size_t size) {
-  acquire(&lk);
-
-  size_t i = 1;
-  while(i < size) i<<=1;
-  
-  head = (void *)(((size_t)head / i + 1) * i); 
-  void *ret = head;
-  head += size;
-
-  release(&lk);
-  if((uintptr_t)head > (uintptr_t)heap.end) return NULL;
+  if(size > PAGE_SIZE) return NULL; //拒绝大内存分配
+  int cpu = cpu_current();
+  int item_id = 1;
+  while(size > (1 << item_id)) item_id++;
+  struct slab *now;
+  if(cache_chain[cpu][item_id] == NULL){
+    acquire(&global_lock);
+    if(head + SLAB_SIZE  > heap.end){
+       release(&global_lock);
+       return NULL;
+    }
+    cache_chain[cpu][item_id] = (struct slab*) head;
+    head += SLAB_SIZE;
+    release(&global_lock);
+    new_slab(cache_chain[cpu][item_id], cpu, get_slab_size(item_id));
+    now = cache_chain[cpu][item_id];
+  } else{
+    now = cache_chain[cpu][item_id];
+    struct slab *walk = NULL;
+    while(now && full_slab(now)) {
+      walk = now;
+      now = now->next;
+    }
+    if(now == NULL) {
+      acquire(&global_lock);
+      if(head + SLAB_SIZE  > heap.end){
+        release(&global_lock);
+        return NULL;
+      }
+      now = (struct slab*) head;
+      head += SLAB_SIZE;
+      release(&global_lock);
+      new_slab(now, cpu, get_slab_size(item_id));
+      walk->next = now;
+    }
+  }
+  if(now == NULL) return NULL;
+  //成功找到slab
+  uint64_t block = 0;
+  for(int i = 0; i < 64; ++i) {
+    if(now->bitmap[i] != UINT64_MAX) {
+      for(int j = 0; j < 64; ++j) {
+        if(now->bitmap[i] & (1ULL << j)) continue;
+        acquire(&lk);
+        now->bitmap[i] |= (1ULL<<j);
+        block = i*64 + j;
+        release(&lk);
+        break;
+      }
+      break;
+    }
+  }
+  void *ret = (void*) ((uintptr_t)((uintptr_t)now + block * get_slab_size(item_id)));
   return ret;
 }
 
 static void kfree(void *ptr) {
-
+  uintptr_t slab_head = ((uintptr_t) ptr / SLAB_SIZE) *SLAB_SIZE;
+  struct slab* sb = (struct slab *)slab_head;
+  uint64_t block = ((uintptr_t)ptr - slab_head) / sb->item_size;
+  uint64_t row = block / 64, col = block % 64;
+  acquire(&lk);
+  sb->bitmap[row] ^= (1ULL << col);
+  release(&lk);
 }
 
 static void pmm_init() {
+  initlock(&global_lock,"GlobalLock");
+  initlock(&lk,"lk");
+  slab_init();
   head = heap.start;
-  initlock(&lk,"GlobalLock");
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 }
