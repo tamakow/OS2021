44ce68cada34dadf20738d0d66fc1dee7306d320
tracer-nju <tracer@nju.edu.cn>
2021-04-18 18:45:19 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 1228da9445f549eeb0d1590e4fe05517   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  18:45:19 up  1:18,  1 user,  load average: 0.24, 0.41, 0.72

--- kernel/test/am.h
+++ kernel/test/am.h
@@ -1,4 +1,7 @@
 #ifndef TEST_AM_H
 #define TEST_AM_H
 
+
+#include <stddef.h>
+
 #endif
--- kernel/test/common.h
+++ kernel/test/common.h
@@ -2,9 +2,8 @@
 #define TEST_COMMON_H
 
 #include <am.h>
-#include <kernel.h>
-#include <klib.h>
-#include <klib-macros.h>
+
+
 #define MiB       *(1 << 20)
 #define HEAP_SIZE 128 * MiB
 
