8947ace79ddfe3e3a1f84d81ec3e794c38764305
tracer-nju <tracer@nju.edu.cn>
2021-04-20 00:17:42 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 83a23221a5d9451294b7edc5aedf32dd   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  00:17:42 up 16:01,  1 user,  load average: 0.69, 0.60, 0.63

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -22,6 +22,7 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  slab_init(heap.start, heap.end);
 }
 #else
 static void pmm_init() {
