20cbd1650bae84e3768366306eebd0d1c7284764
tracer-nju <tracer@nju.edu.cn>
2021-04-20 23:10:08 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0dcd4d7f243841b29ef79edb919e6d2a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  23:10:08 up  8:42,  1 user,  load average: 1.22, 1.43, 1.51

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -97,12 +97,12 @@ void Init_Item(struct slab *sb, struct item *it) {
     else {
       //也可以直接插在链表头
       // acquire(&sb->cache->lock);
-      it->next = sb->items;
-      sb->items = it;
+      // it->next = sb->items;
+      // sb->items = it;
       // release(&sb->cache->lock);
-      // struct item *walk = sb->items;
-      // while(walk->next) walk = walk->next;
-      // walk->next = it;
+      struct item *walk = sb->items;
+      while(walk->next) walk = walk->next;
+      walk->next = it;
     }
 }
 
@@ -154,11 +154,11 @@ bool New_Slab (struct kmem_cache* cache) {
   if(cache->slabs_free == NULL) cache->slabs_free = sb;
   else {
     //也可以直接插在链表头
-    sb->next = cache->slabs_free;
-    cache->slabs_free = sb;  
-    // struct slab* walk = cache->slabs_free;
-    // while(walk->next) walk = walk->next;
-    // walk->next = sb;
+    // sb->next = cache->slabs_free;
+    // cache->slabs_free = sb;  
+    struct slab* walk = cache->slabs_free;
+    while(walk->next) walk = walk->next;
+    walk->next = sb;
   }
 
   return true;
