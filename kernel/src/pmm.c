b3c5a92ae22a909c12de482cd242cee313361f8c
tracer-nju <tracer@nju.edu.cn>
2021-04-20 22:35:49 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0dcd4d7f243841b29ef79edb919e6d2a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  22:35:49 up  8:08,  1 user,  load average: 1.51, 1.54, 1.46

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -57,7 +57,7 @@ void Init_Kmem_Cache (struct kmem_cache * cache, size_t size){
     // 大部分小于 128 KiB
     cache->slab_alloc_pages = 1;
     cache->slab_max_item_nr = (PAGE_SIZE * 2 - sizeof(struct slab)) / size; 
-  } else if (size <= PAGE_SIZE * 2) {
+  } else if (size <= PAGE_SIZE) {
     // 大内存分配四个就够了,减1的目的是确保之后的加1不会出错
     if(cpu_count() == 4 && size == PAGE_SIZE * 2){
       cache->slab_alloc_pages = (size * 16 + sizeof(struct slab) - 1) / PAGE_SIZE + 1; 
