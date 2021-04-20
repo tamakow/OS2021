4b2465b81aba01678170ef2a48ce13ce2babd220
tracer-nju <tracer@nju.edu.cn>
2021-04-20 17:44:50 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 0dcd4d7f243841b29ef79edb919e6d2a   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-50-generic       Architecture: x86-64  17:44:50 up  3:17,  1 user,  load average: 0.72, 0.60, 0.76

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -116,9 +116,9 @@ struct kmem_cache* Find_Kmem_Cache(size_t size) {
  */
 
 bool New_Slab (struct kmem_cache* cache) {
-  acquire(&cache->lock);
+  // acquire(&cache->lock);
   void * freehead = alloc_pages(cache->slab_alloc_pages);
-  release(&cache->lock);
+  // release(&cache->lock);
   if(freehead == NULL) {
     Log("No enough space to allocate a new slab of %d pages", cache->slab_alloc_pages);
     return false;
