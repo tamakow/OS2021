9ffb66569675e1526e9948fb70dbde986bdae9d8
tracer-nju <tracer@nju.edu.cn>
2021-05-17 01:41:03 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 8b08a53fd7934ca18f65cdbd86913d69   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  01:41:03 up 16:22,  1 user,  load average: 1.21, 1.49, 1.37

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -10,7 +10,40 @@
 
 
 // #define DEBUG
-// #include "debug.h"
+
+#define  FONT_BLACK          "\033[1;30m"
+#define  FONT_RED            "\033[1;31m"
+#define  FONT_GREEN          "\033[1;32m"
+#define  FONT_YELLOW         "\033[1;33m"
+#define  FONT_BLUE           "\033[1;34m"
+#define  FONT_PURPLE         "\033[1;35m"
+#define  FONT_CYAN           "\033[1;36m"
+#define  FONT_WHITE          "\033[1;37m"
+
+#define  FONT_RESET          "\033[0m"
+
+#ifdef  DEBUG 
+#define Log(format, ...) \
+    printf(FONT_BLUE "[%s,%d,%s] " format FONT_RESET"\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#define print(FONT_COLOR, format, ...) \
+    printf(FONT_COLOR format FONT_RESET "\n", \
+        ## __VA_ARGS__)
+#else
+#define Log(format, ...)
+#define print(FONT_COLOR, format, ...)
+#endif
+
+
+#ifdef  DEBUG
+#define Assert(color, format, ...)\
+    print(color, format,##  __VA_ARGS__);\
+    assert(0)
+#else
+#define Assert(color, format, ...)\
+    assert(0)
+#endif
+
 
 #define NAME_LEN 64
 #define TIME_LEN 64
