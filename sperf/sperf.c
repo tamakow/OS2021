d13a0d6aa3f3a1b583ec5a560b41996c5bacff47
tracer-nju <tracer@nju.edu.cn>
2021-05-16 12:12:12 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 8b08a53fd7934ca18f65cdbd86913d69   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  12:12:13 up  2:53,  1 user,  load average: 0.75, 0.66, 0.68

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,12 +8,8 @@
 #define DEBUG
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "strace", "ls", NULL, };
-  char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
-  perror(argv[0]);
-  exit(EXIT_FAILURE);
-  
+  if(argc < 2) {
+    print(FONT_CYAN, "Invalid arguements\n"
+                    "Usage: ./sperf-64 [cmd] [args]\n");
+  }
 }
