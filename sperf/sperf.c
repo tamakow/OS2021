64be59f6082d58121ed4f44491cc7ce3388ef00b
tracer-nju <tracer@nju.edu.cn>
2021-05-16 19:58:03 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 8b08a53fd7934ca18f65cdbd86913d69   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  19:58:03 up 10:39,  1 user,  load average: 1.15, 1.18, 1.35

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -59,11 +59,11 @@ int main(int argc, char *argv[]) {
 
   if(pid == 0) { 
     close(fildes[0]);
-    int blackhole = open("/dev/null", O_RDWR | O_APPEND);
-    if(blackhole == -1){ 
-      Assert(FONT_RED, "Open /dev/null failed");
-    }
-    dup2(blackhole, STDOUT_FILENO);
+    // int blackhole = open("/dev/null", O_RDWR | O_APPEND);
+    // if(blackhole == -1){ 
+    //   Assert(FONT_RED, "Open /dev/null failed");
+    // }
+    // dup2(blackhole, STDOUT_FILENO);
     dup2(fildes[1], STDERR_FILENO); 
     // strace must be in some place in the ath
     strcat(exec_path, "strace");
