061c1988cd1923ee252bb65a2fde32f59c0de594
tracer-nju <tracer@nju.edu.cn>
2021-05-17 15:12:26 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 24267ced44d546feb066169d643e4c61   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  15:12:26 up 33 min,  1 user,  load average: 0.48, 0.37, 0.37

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -186,17 +186,16 @@ int main(int argc, char *argv[]) {
 
       //读取系统调用名称
       if(regexec(&name_preg, str, 1, &name_match, 0) == REG_NOMATCH) {
-        // continue;
-        Assert(FONT_BLUE, "No match for name");
+        continue;
+        // Assert(FONT_BLUE, "No match for name");
       }
       strncpy(name, str + name_match.rm_so, name_match.rm_eo - name_match.rm_so);
       name[name_match.rm_eo - name_match.rm_so - 1] = '\0';
-      if(strcmp(name, "exit_group") == 0) break;
       //读取系统调用时间
       Log("name is %s",str);
       if(regexec(&time_preg, str, 1, &time_match, 0) == REG_NOMATCH) {
-        // continue;
-        Assert(FONT_BLUE, "No match for time");
+        continue;
+        // Assert(FONT_BLUE, "No match for time");
       }
       strncpy(time, str + time_match.rm_so + 1, time_match.rm_eo - time_match.rm_so - 2);
       time[time_match.rm_eo - time_match.rm_so - 3] = '\0';
