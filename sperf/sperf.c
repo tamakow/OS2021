fcdeeb7d5c98f220f53029fd353516f4a8aaf886
tracer-nju <tracer@nju.edu.cn>
2021-05-17 10:56:38 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 988743541a7f4e49a1d05c8f5418ffdb   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  10:56:38 up  1:51,  1 user,  load average: 0.50, 0.51, 0.59

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -79,13 +79,20 @@ void display() {
   bubble_sort();
   syscall_node_t *walk = head;
   for(int i = 0; i < 5 && walk != NULL;i++, walk = walk->next) {
-    printf("%s (%d%%)\n",walk->name, (int)((walk->time * 100) / total_time));
+    char tmp[NAME_LEN];
+    strcpy(tmp, walk->name);
+    for (int j = 0; j < strlen(tmp); ++j) {
+      if(tmp[j] >= 'A' && tmp[j] <= 'Z') {
+        tmp[j] = tmp[j] - 'A' + 'a';
+      }
+    }
+    printf("%s (%d%%)\n",tmp, (int)((walk->time * 100) / total_time));
   }
   printf("====================\n");
   for(int i = 0; i < 80; ++i)
     putchar('\0');
-  // fflush(stdout);
-  // fflush(stderr);
+  fflush(stdout);
+  fflush(stderr);
 }
 
 
