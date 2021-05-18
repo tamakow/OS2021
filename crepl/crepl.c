b87b52bfad9904ed89b5efb56b7a4f3ecc726943
tracer-nju <tracer@nju.edu.cn>
2021-05-18 20:31:04 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e8eac9575a5a4957a51b402cd8216938   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  20:31:04 up  6:25,  1 user,  load average: 0.57, 0.29, 0.17

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -4,16 +4,50 @@
 #include <unistd.h>
 #include <dlfcn.h>
 
+// #define DEBUG
 
+#define  FONT_BLACK          "\033[1;30m"
+#define  FONT_RED            "\033[1;31m"
+#define  FONT_GREEN          "\033[1;32m"
+#define  FONT_YELLOW         "\033[1;33m"
+#define  FONT_BLUE           "\033[1;34m"
+#define  FONT_PURPLE         "\033[1;35m"
+#define  FONT_CYAN           "\033[1;36m"
+#define  FONT_WHITE          "\033[1;37m"
+
+#define  FONT_RESET          "\033[0m"
+
+#ifdef  DEBUG 
+#define Log(format, ...) \
+    printf(FONT_BLUE "[%s,%d,%s] " format FONT_RESET"\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#define print(FONT_COLOR, format, ...) \
+    printf(FONT_COLOR format FONT_RESET "\n", \
+        ## __VA_ARGS__)
+#else
+#define Log(format, ...)
+#define print(FONT_COLOR, format, ...)
+#endif
+
+
+#ifdef  DEBUG
+#define Assert(format, ...)\
+    Log(format,##  __VA_ARGS__);\
+    assert(0)
+#else
+#define Assert(format, ...)\
+    assert(0)
+#endif
+
+
+static char line[4096];
 
 
 int main(int argc, char *argv[]) {
-  static char line[4096];
   while (1) {
     printf(">> ");
     fflush(stdout);
     if (!fgets(line, sizeof(line), stdin)) {
-      
       break;
     }
     printf("Got %zu chars.\n", strlen(line)); // ??
