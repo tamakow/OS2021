cc4539a2ab9f597ba4d4e71bf9a6dc2b59d097fa
tracer-nju <tracer@nju.edu.cn>
2021-05-18 21:48:13 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e8eac9575a5a4957a51b402cd8216938   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  21:48:13 up  7:42,  1 user,  load average: 0.49, 0.53, 0.50

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -71,6 +71,6 @@ int main(int argc, char *argv[]) {
     static char tmp_c_file[] = "/tmp/tmp_c_XXXXXX";
     static char tmp_so_file[] = "/tmp/tmp_so_XXXXXX";
 
-    Assert(mkstemp(tmp_c_file) < 0 || mkstemp(tmp_so_file) < 0, "Create tmp file failed!");
+    Assert(mkstemp(tmp_c_file) < 0 || mkstemp(tmp_so_file) < 0, "Create tmp file failed! %d, %d",mktemp(tmp_c_file),mktemp(tmp_so_file));
   }
 }
