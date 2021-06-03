bffc848763b6a4432c523deb651bde7a1af6b498
tracer-nju <tracer@nju.edu.cn>
2021-06-03 09:45:55 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: e21544d2d27b43a29e6812f867af59c4   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  09:45:55 up  9:33,  1 user,  load average: 0.47, 0.48, 0.36

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,8 +1,93 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+
+#define bool uint8_t
+#define false 0
+#define true 1
+
+#define DEBUG
+
+#define  FONT_BLACK          "\033[1;30m"
+#define  FONT_RED            "\033[1;31m"
+#define  FONT_GREEN          "\033[1;32m"
+#define  FONT_YELLOW         "\033[1;33m"
+#define  FONT_BLUE           "\033[1;34m"
+#define  FONT_PURPLE         "\033[1;35m"
+#define  FONT_CYAN           "\033[1;36m"
+#define  FONT_WHITE          "\033[1;37m"
+
+#define  FONT_RESET          "\033[0m"
+
+#define print(FONT_COLOR, format, ...) \
+    printf(FONT_COLOR format FONT_RESET "\n", \
+        ## __VA_ARGS__)
+
+#ifdef  DEBUG 
+#define Log(format, ...) \
+    printf(FONT_BLUE "[%s,%d,%s] " format FONT_RESET"\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+#else
+#define Log(format, ...)
+#endif
+
+#ifdef  DEBUG
+  #define Assert(cond, format, ...) \
+    do { \
+      if (!(cond)) { \
+        Log(format, ## __VA_ARGS__); \
+        exit(1); \
+      } \
+    } while (0)
+#else
+#define Assert(cond, format, ...)\
+    do { \
+      if (!(cond)) { \
+        exit(1); \
+      } \
+    } while (0)
+#endif
+
+
+
+
+
+struct fat_header {
+    uint8_t BS_jmpBoot[3];
+    uint8_t BS_OEMName[8];
+    uint16_t BPB_BytsPerSec;
+    uint8_t BPB_SecPerClus;
+    uint16_t BPB_RsvdSecCnt;
+    uint8_t BPB_NumFATs;
+    uint16_t BPB_RootEntCnt;
+    uint16_t BPB_TotSec16;
+    uint8_t BPB_Media;
+    uint16_t BPB_FATSz16;
+    uint16_t BPB_SecPerTrk;
+    uint16_t BPB_NumHeads;
+    uint32_t BPB_HiddSec;
+    uint32_t BPB_TotSec32;
+    uint32_t BPB_FATSz32;
+    uint16_t BPB_ExtFlags;
+    uint16_t BPB_FSVer;
+    uint32_t BPB_RootClus;
+    uint16_t BPB_FSInfo;
+    uint16_t BPB_BkBootSec;
+    uint8_t BPB_Reserved[12];
+    uint8_t BS_DrvNum;
+    uint8_t BS_Reserved1;
+    uint8_t BS_BootSig;
+    uint32_t BS_VollD;
+    uint8_t BS_VolLab[11];
+    uint8_t BS_FilSysType[8];
+    uint8_t  padding[420];
+    uint16_t Signature_word;
+}__attribute__((packed));
+
 
 int main(int argc, char *argv[]) {
-    for (int i = 0; i < 20; ++i) {
-        printf("hello world!\n");
-    }
-    return 0;
+    Assert(sizeof(struct fat_header) != 512, "bad header!");
+    
 }
