0d1cff010e7b704d80203071fe291cb69e491287
tracer-nju <tracer@nju.edu.cn>
2021-06-09 19:49:30 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b0b74d6d11004b9483de043a9dd114b3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  19:49:30 up  4:58,  1 user,  load average: 0.56, 0.56, 0.56

--- kernel/src/slab.c
+++ kernel/src/slab.c
@@ -29,6 +29,7 @@ void new_slab(slab * sb, int cpu, int item_id) {
     // init obj_head;
     // 只在未分配的obj上有用，已分配的无所谓
     uintptr_t offset = 0;
+    Log("ok");
     acquire(&sb->lock);
     while(sb->start_ptr + offset <= (uintptr_t)sb + PAGE_SIZE) {
         struct obj_head* objhead = (struct obj_head*) (sb->start_ptr + offset);
