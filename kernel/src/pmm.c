7306b13d7672bc290a5665f79efe19cef7aed19c
tracer-nju <tracer@nju.edu.cn>
2021-06-09 16:37:12 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b0b74d6d11004b9483de043a9dd114b3   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  16:37:12 up  1:45,  1 user,  load average: 0.96, 1.04, 1.00

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -80,12 +80,12 @@ static void *kalloc(size_t size) {
   Log("now offset is %p", now->offset);
   Log("now ptr is %p", now_ptr);
   //分配完，更新最新的offset，并更新它的下一个offset
-  struct obj_head *objhead = (struct obj_head *)now_ptr;
+  // struct obj_head *objhead = (struct obj_head *)now_ptr;
   
   //在不free的时候可以这么用
   Log("Havn't use this slab, the offset is %p %d", now->offset, now->offset);
-  objhead->next_offset = now->offset + (1 << now->obj_order);
-  now->offset = objhead->next_offset;
+  // objhead->next_offset = now->offset + (1 << now->obj_order);
+  now->offset = now->offset + (1 << now->obj_order);
   
   Log("Ready to judge if now is full");
   if(full_slab(cache_chain[cpu][item_id])) { //已经满了
