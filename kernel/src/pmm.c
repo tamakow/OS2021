b201dbea9e35bcacc488a098ad1b7da18849fce9
tracer-nju <tracer@nju.edu.cn>
2021-06-10 08:28:32 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 3162c2d65d214808b11b95c06a1e9a88   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  08:28:32 up 20 min,  1 user,  load average: 0.89, 0.83, 0.55

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -98,7 +98,6 @@ static void *kalloc(size_t size) {
 
 //只是回收了slab中的对象，如果slab整个空了无法回收
 static void kfree(void *ptr) {
-  return;
   if((uintptr_t)ptr >= (uintptr_t)tail) return; //大内存不释放
   uintptr_t slab_head = ((uintptr_t) ptr / PAGE_SIZE) * PAGE_SIZE;
   Log("slabhead is %p", slab_head);
