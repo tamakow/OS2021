1f488d0c1e8872f43ef564f13f9912d81b8a16c1
tracer-nju <tracer@nju.edu.cn>
2021-07-07 16:20:17 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 7710bab69744461691ddaeaa85523c5f   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  16:20:17 up  3:30,  1 user,  load average: 0.94, 0.64, 0.58

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -53,6 +53,23 @@
     } while (0)
 #endif
 
+
+#define  ATTR_READ_ONLY 0x01
+#define  ATTR_HIDDEN    0x02
+#define  ATTR_SYSTEM    0x04
+#define  ATTR_VOLUME_ID 0x08
+#define  ATTR_DIRECTORY 0x10
+#define  ATTR_ARCHIVE   0x20
+
+#define ATTR_LONG_NAME \
+ (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID)
+
+#define ATTR_LONG_NAME_MASK \
+    (ATTR_READ_ONLY | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID | \
+        ATTR_DIRECTORY | ATTR_ARCHIVE)
+
+// bios paremeter block(BPB)
+// the first sector of the volume
 struct fat_header {
     uint8_t BS_jmpBoot[3];
     uint8_t BS_OEMName[8];
@@ -68,12 +85,13 @@ struct fat_header {
     uint16_t BPB_NumHeads;
     uint32_t BPB_HiddSec;
     uint32_t BPB_TotSec32;
+    // Extended BPB structure for FAT32 volumes
     uint32_t BPB_FATSz32;
     uint16_t BPB_ExtFlags;
     uint16_t BPB_FSVer;
     uint32_t BPB_RootClus;
-    uint16_t BPB_FSInfo;
-    uint16_t BPB_BkBootSec;
+    uint16_t BPB_FSInfo; // sector number of FSINFO structure
+    uint16_t BPB_BkBootSec; //BPB的备份所在的sector FAT32中一般是6
     uint8_t BPB_Reserved[12];
     uint8_t BS_DrvNum;
     uint8_t BS_Reserved1;
@@ -85,6 +103,54 @@ struct fat_header {
     uint16_t Signature_word;
 } __attribute__((packed));
 
+struct fat_dir {
+  uint8_t DIR_Name[11]; //short name (limited to 11 characters)
+  uint8_t DIR_Attr;
+  uint8_t DIR_NTRes;
+  uint8_t DIR_CrtTimeTenth;
+  uint16_t DIR_CrtTime;
+  uint16_t DIR_CrtDate;
+  uint16_t DIR_LstAccDate;
+  uint16_t DIR_FstClusHI;
+  uint16_t DIR_WrtTime;
+  uint16_t DIR_WrtDate;
+  uint16_t DIR_FstClusLO;
+  uint32_t DIR_FileSize;
+ } __attribute__((packed));
+
+struct fat_ldir {
+  uint8_t LDIR_Ord;
+  uint8_t LDIR_Name1[10];
+  uint8_t LDIR_Attr;
+  uint8_t LDIR_Type;
+  uint8_t LDIR_Chksum;
+  uint8_t LDIR_Name2[12];
+  uint16_t LDIR_FstClusLO;
+  uint8_t LDIR_Name3[4];
+} __attribute__((packed));
+
+
+// http://www.ece.ualberta.ca/~elliott/ee552/studentAppNotes/2003_w/misc/bmp_file_format/bmp_file_format.htm
+struct bmp_header {
+  uint16_t Signature; // 'BM'
+  uint32_t FileSize; // File size in bytes
+  uint32_t reserved; //0
+  uint32_t DataOffset;
+} __attribute__((packed));
+
+struct bmp_infoheader {
+  uint32_t Size;
+  int32_t  Width;  
+  int32_t  Height; 
+  uint16_t Planes;
+  uint16_t BitsPerPixel;
+  uint32_t Compression;
+  uint32_t ImageSize;
+  int32_t  XpixelsPerM;
+  int32_t  YpixelsPerM;
+  uint32_t ColorsUsed;
+  uint32_t ImportantColors;
+} __attribute__((packed));
 
 void Usage() {
   printf("Invalid usage\n");
@@ -95,18 +161,25 @@ void Usage() {
 int main(int argc, char *argv[]) {
     Assert(sizeof(struct fat_header) == 512, "bad header!");
     Log("%d", (int)sizeof(struct fat_header));
+    
     if(argc < 2) {
       Usage();
       exit(EXIT_FAILURE);
     }
+
     char *img = argv[1];
     int fd = open(img, O_RDONLY);
-    Assert(fd, "open img file failed!");
-    struct fat_header *disk = (struct fat_header *)mmap(NULL, sizeof(struct fat_header), PROT_READ, MAP_PRIVATE, fd, 0);  
-    Log("Bytes per sector is %d,the number of sectors per cluster is %d", (int)disk->BPB_BytsPerSec, (int)disk->BPB_SecPerClus);
-    Log("the number of FAT is %d", (int)disk->BPB_NumFATs);
-    Assert(disk->Signature_word == 0xaa55, "not a valid fat");
-    munmap((void *)disk, sizeof(struct fat_header));
+    Assert(fd != -1, "open img file failed!");
+    struct stat st;
+    Assert(fstat(fd, &st) != -1, "Read img stat failed!");
+    
+    void *FAT_HEAD = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);  
+    
+    struct fat_header* BPB =  (struct fat_header *)FAT_HEAD;
+    Log("Bytes per sector is %d,the number of sectors per cluster is %d", (int)BPB->BPB_BytsPerSec, (int)BPB->BPB_SecPerClus);
+    Log("the number of FAT is %d", (int)BPB->BPB_NumFATs);
+    Assert(BPB->Signature_word == 0xaa55, "not a valid fat");
+    munmap(FAT_HEAD, st.st_size);
     close(fd);
     
     return 0;
