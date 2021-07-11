64c8c83fd7b6f82b391a1d96facf39deda9bf2c9
tracer-nju <tracer@nju.edu.cn>
2021-07-11 22:59:59 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b247a899349a4ed2814638bf84244eec   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  22:59:59 up  2:04,  1 user,  load average: 0.92, 1.25, 1.08

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -136,15 +136,7 @@ static void kfree(void *ptr) {
   Log("slabhead is %p", slab_head);
   slab* sb = (slab *)slab_head;
   struct obj_head* objhead = (struct obj_head*) ptr;
-  acquire(&sb->lock);
-  sb->obj_cnt--;
-  Log("objcnt is %d", sb->obj_cnt);
-  objhead->next_offset = sb->offset;
-  Log("next_offset is %d", objhead->next_offset);
-  sb->offset = (uintptr_t)ptr - (uintptr_t)sb->start_ptr;
-  Log("sb->offset is %d", sb->offset);
-  release(&sb->lock);
-  if(sb->obj_cnt == 0) {
+  if(sb->obj_cnt == 1) {
     acquire(&global_lock);
     struct freelist *empty = (struct freelist*)sb;
     if(head != NULL) {
@@ -156,8 +148,17 @@ static void kfree(void *ptr) {
       head->next = NULL;
     }
     release(&global_lock);
+    return;
   }
-  else insert_slab_to_head(sb);
+  acquire(&sb->lock);
+  sb->obj_cnt--;
+  Log("objcnt is %d", sb->obj_cnt);
+  objhead->next_offset = sb->offset;
+  Log("next_offset is %d", objhead->next_offset);
+  sb->offset = (uintptr_t)ptr - (uintptr_t)sb->start_ptr;
+  Log("sb->offset is %d", sb->offset);
+  release(&sb->lock);
+  insert_slab_to_head(sb);
 }
 
 static void pmm_init() {
