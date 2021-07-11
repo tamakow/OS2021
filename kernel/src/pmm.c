1efd914c35772e791954801e635eb59982a5e0c8
tracer-nju <tracer@nju.edu.cn>
2021-07-11 22:50:46 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: b247a899349a4ed2814638bf84244eec   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  22:50:46 up  1:55,  1 user,  load average: 0.81, 0.68, 0.73

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -171,9 +171,10 @@ static void pmm_init() {
   struct freelist *walk = head;
   int cnt = 0;
   printf("%d\n",(int)((uintptr_t)big_alloc_head - (uintptr_t)head) / PAGE_SIZE);
-  while((uintptr_t)(walk + (1 << 15)) < (uintptr_t)big_alloc_head) {
-    walk->next = (void *)(walk + (1 << 15));
-    printf("%p %p\n", walk, walk->next);
+  printf("%p\n", PAGE_SIZE);
+  while((uintptr_t)(walk + PAGE_SIZE) < (uintptr_t)big_alloc_head) {
+    walk->next = (void *)(walk + PAGE_SIZE);
+    // printf("%p %p\n", walk, walk->next);
     cnt++;
     walk = (struct freelist *)walk->next;
   }
