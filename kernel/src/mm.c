38363d59416e27a57f77184ea36093deb1507a6c
tracer-nju <tracer@nju.edu.cn>
2021-07-29 15:55:13 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 68c4409818894be890433725accf0740   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  15:55:13 up 26 min,  1 user,  load average: 0.67, 0.68, 0.61

--- kernel/src/mm.c
+++ kernel/src/mm.c
@@ -31,10 +31,17 @@ void new_page(page_t *sb, int cpu, int item_id) {
     // init obj_head;
     // 只在未分配的obj上有用，已分配的无所谓
     uintptr_t offset = 0;
-    while(sb->start_ptr + offset + size <= (uintptr_t)sb + PAGE_SIZE) {
+    // while(sb->start_ptr + offset + size <= (uintptr_t)sb + PAGE_SIZE) {
+    //     struct obj_head* objhead = (struct obj_head*) (sb->start_ptr + offset);
+    //     offset += size;
+    //     objhead->next_offset = offset;
+    // }
+    while(sb->start_ptr + offset < (uintptr_t)sb + PAGE_SIZE) {
         struct obj_head* objhead = (struct obj_head*) (sb->start_ptr + offset);
+        objhead->next_offset = offset + size;
         offset += size;
-        objhead->next_offset = offset;
+        if(sb->start_ptr + offset > (uintptr_t)sb + PAGE_SIZE)
+            break;
     }
 #ifdef DEBUG
     struct obj_head* objhead = (struct obj_head*) sb->start_ptr;
