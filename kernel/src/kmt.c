a0cc7c35c1d4e7f64655e3489f8ade73ccae7bb5
tracer-nju <tracer@nju.edu.cn>
2021-07-29 16:38:33 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 68c4409818894be890433725accf0740   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  16:38:33 up  1:09,  1 user,  load average: 0.43, 0.58, 0.62

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -86,7 +86,7 @@ void kmt_init() {
         current[i] = &idle[i];
     }
     
-    os->on_irq(INT_MIN, EVENT_NULL, kmt_context_save);
+    os->on_irq(-9999999, EVENT_NULL, kmt_context_save);
     os->on_irq(INT_MAX, EVENT_NULL, kmt_schedule);
 }
 
