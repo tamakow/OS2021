f631a0551a9f29f873f53642f7e613a48bb8ffff
tracer-nju <tracer@nju.edu.cn>
2021-07-29 16:09:38 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 68c4409818894be890433725accf0740   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  16:09:38 up 40 min,  1 user,  load average: 0.99, 0.80, 0.65

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -14,7 +14,6 @@ void initlock(struct spinlock *lk,const char* name) {
 void acquire(struct spinlock *lk) {
     pushcli(); //avoid deadlock
     if(holding(lk)) panic("acquire");
-    Log("%d", lk->locked);
     while(atomic_xchg((int *)&lk->locked, 1) != 0) ;
     __sync_synchronize();
     lk->cpu = cpu_current();
