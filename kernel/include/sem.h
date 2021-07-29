820ece5b8e274ff469e35bb090ce68a6811b0f1d
tracer-nju <tracer@nju.edu.cn>
2021-07-29 17:56:43 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 68c4409818894be890433725accf0740   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  17:56:43 up  2:28,  1 user,  load average: 1.09, 0.60, 0.59

--- kernel/include/sem.h
+++ kernel/include/sem.h
@@ -1,14 +1,11 @@
 #ifndef __SEM_H__
 #define __SEM_H__
 
-#define QLEN 64
 
 struct semaphore {
   struct spinlock lock;
   const char *name;
   int value;
-  int head_idx, tail_idx;
-  task_t* queue[QLEN];
 };
 
 void sem_init(sem_t *, const char *, int);
--- kernel/src/sem.c
+++ kernel/src/sem.c
@@ -8,33 +8,12 @@ void sem_init(sem_t *sem, const char *name, int value){
     initlock(&sem->lock, "sem_lock");
     sem->name = name;
     sem->value = value;
-    sem->head_idx = 0;
-    sem->tail_idx = 0;
 }
 
 void sem_wait(sem_t *sem){
-    bool flag = false;
-    acquire(&sem->lock);
-    --sem->value;
-    if(sem->value < 0) {
-        flag = true;
-        Current->state = BLOCKED;
-        sem->queue[sem->tail_idx] = Current;
-        sem->tail_idx = (sem->tail_idx + 1) % QLEN;
-        panic_on(sem->tail_idx == sem->head_idx, "sem_wait queue overflow!");
-    }
-    release(&sem->lock);
-    if(flag) yield();
+    return;
 }
   
 void sem_signal(sem_t *sem){
-    acquire(&sem->lock);
-    ++sem->value;
-    if (sem->value <= 0){
-        if(sem->head_idx != sem->tail_idx) {
-            sem->queue[sem->head_idx]->state = RUNNABLE;
-            sem->head_idx = (sem->head_idx + 1) % QLEN;
-        }
-    }
-    release(&sem->lock);
+    return;
 }
