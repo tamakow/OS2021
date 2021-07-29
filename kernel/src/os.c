af45e1116e4983099462006dbdd3afe56b6ec3a4
tracer-nju <tracer@nju.edu.cn>
2021-07-29 16:36:08 +0800

    Static hostname: tamako-Inspiron-7590          Icon name: computer-laptop            Chassis: laptop         Machine ID: 418affb9e5a34d45aa5ed13d24407458            Boot ID: 68c4409818894be890433725accf0740   Operating System: Ubuntu 20.04.1 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  16:36:09 up  1:07,  1 user,  load average: 0.64, 0.66, 0.65

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -22,9 +22,7 @@ static void os_init() {
   KLog("pmm init ok!");
   kmt->init();
   KLog("kmt init ok!");
-  // dev->init();
-  // dev->init();
-  // dev->init();
+  dev->init();
   // kmt->sem_init(&empty, "empty", 5);  // 缓冲区大小为 5
   // kmt->sem_init(&fill,  "fill",  0);
   // for (int i = 0; i < 4; i++) // 4 个生产者
